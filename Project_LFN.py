b49a2297b2e890997e18887fca6cbf2d4c5c3f12
LucaScattolaro <lucascattolaro98@gmail.com>
2021-10-31 16:45:39 +0100

    Frist Commit

--- Project_LFN.py
+++ Project_LFN.py
@@ -1,4 +1,14 @@
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import glob
+import pandas as pd
 
-print('ciao')
-print('commit - alberto')
+twitchGraph = nx.Graph()
+#--Nodes
+twitchNodes = pd.read_csv('twitch_gamers/large_twitch_features.csv')
+print(twitchNodes)
+
+#--Edges
+twitchEdges = pd.read_csv('twitch_gamers/large_twitch_edges.csv')
+print(twitchEdges)
