5e17c166c5c5b2b9cf40272ade7488855e6bf945
LucaScattolaro <lucascattolaro98@gmail.com>
2021-11-18 14:19:32 +0100

    local clustering coeff

--- Project_LFN.py
+++ Project_LFN.py
@@ -3,12 +3,49 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import glob
 import pandas as pd
+import graphLibrary
+import time
+import csv
+import collections
 
+
+########       CREATION OF THE GRAPH
 twitchGraph = nx.Graph()
 #--Nodes
-twitchNodes = pd.read_csv('twitch_gamers/large_twitch_features.csv')
-print(twitchNodes)
-
+#twitchNodes = pd.read_csv('twitch_gamers/large_twitch_features.csv')
 #--Edges
 twitchEdges = pd.read_csv('twitch_gamers/large_twitch_edges.csv')
-print(twitchEdges)
+#--Creation of the Graph
+twitchGraph = nx.from_pandas_edgelist(twitchEdges, 'numeric_id_1', 'numeric_id_2')
+
+
+########       LOCAL CLUSTERING COEFFICIENT FOR ALL NODES
+#--Compute local clustering coefficient and save it into a Dictionary in order to save it later
+start=time.time()
+nodeLCC_dictionary={}
+i=0
+for node in list(twitchGraph.nodes):
+    start=time.time()
+    deg_v,lcc_v=graphLibrary.LCC(twitchGraph,node)
+    nodeLCC_dictionary[node]={'degree':deg_v,'local_CC':lcc_v}
+    print('iteration: ',i,'    --->    time:  ',time.time()-start)
+    i=i+1
+
+
+print('Order Dictionary')
+nodeLCC_dictionaryOrdered=collections.OrderedDict(sorted(nodeLCC_dictionary.items()))
+
+#--Create the csv writer that will contain the data ragarding local clustering coefficients of all nodes
+
+print('Write Data of Dictionary')
+f = open('localClusteringCoefficients.csv', 'w')
+writer = csv.writer(f)
+#--Write the Header to the csv file
+writer.writerow(['node', 'degree','local_CC'])
+#--Write the Data of nodes to the csv file
+for k, v in nodeLCC_dictionaryOrdered.items(): 
+    writer.writerow([k, v['degree'],v['local_CC']])
+
+#--Close the file
+f.close()
+
