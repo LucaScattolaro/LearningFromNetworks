8056212d32ca332e30a26c55a4465651ab3c5f0f
LucaScattolaro <lucascattolaro98@gmail.com>
2021-11-19 17:15:31 +0100

    global clustering coefficient

--- graphLibrary.py
+++ graphLibrary.py
@@ -2,13 +2,11 @@ from networkx.classes.function import neighbors
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-import glob
-import pandas as pd
 import random
+import scipy.special
 
 
 #########        Clustering Coefficient Methods
-
 def LCC(G,v):
     #--Calculate the exact local clustering coefficient of a node v of grapgh G
     num_t=0
@@ -24,11 +22,11 @@ def LCC(G,v):
 
 def LCCs(G):
     #--Calculate the exact local clustering coefficient for all the nodes of grapgh G
-    ccs={}
+    lccs={}
     for node in list(G.nodes):
-        deg, cc=LCC(G,node)
-        ccs[node]={'degree':deg, 'localCC':cc}
-    return ccs
+        deg, lcc=LCC(G,node)
+        lccs[node]={'degree':deg, 'localCC':lcc}
+    return lccs
 
 def EstimateLCCs(G,k):
     #--Calculate the approximate local clustering coefficient for all the nodes of graph G
@@ -55,7 +53,7 @@ def EstimateLCCs(G,k):
             if min_v[edge[0]]==min_v[edge[0]]:
                 Z[frozenset(edge)]=Z[frozenset(edge)]+1
 
-    ccs={}
+    lccs={}
     for node in nodes:
         neighbors_v=list(G.neighbors(node))
         num_neigh=len(neighbors_v)
@@ -64,18 +62,20 @@ def EstimateLCCs(G,k):
         for u in neighbors_v:
             sum=sum+((Z[frozenset(u,node)]/(Z[frozenset(u,node)]+k))*(num_neigh+G.neighbors(u)))
         
-        ccs[node]=(0.5*sum)/(num_neigh*(num_neigh-1))
+        lccs[node]=(0.5*sum)/(num_neigh*(num_neigh-1))
 
-    return ccs
-
-
+    return lccs
 
 
+def globalClusteringCoefficient(G):
+    #--Calculate the exact local clustering coefficient for all the nodes of grapgh G
+    num_t=0
+    nodes=list(G.nodes)
+    for node in nodes:
+        neighbors_v=list(G.neighbors(node))
+        for u1 in neighbors_v:
+            for u2 in neighbors_v:
+                if u1!=u2 and G.has_edge(u1,u2):
+                    num_t=num_t+1
 
-    
-
-
-
-
-def globalClusteringCoefficient():
-    print('global Clustering Coefficient')
+    return num_t/(6*scipy.special.binom(len(nodes), 3))
